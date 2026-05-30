42ea570f7fc87841f1ace96352de15352b17512c
Matthew Akrigg <matthewakrigg05@gmail.com>
2026-05-30 16:11:49 +0100

    Repo interface

--- src/SplitCents.Core/Services/UserService.cs
+++ src/SplitCents.Core/Services/UserService.cs
@@ -1,7 +1,46 @@
 namespace SplitCents.Core.Services;
+using SplitCents.Core.Interfaces.Repositories;
 using SplitCents.Core.Interfaces.Services;
+using SplitCents.Core.Models;
 
 public class UserService : IUserService
 {
-    
+    private readonly IUserRepository _users;
+
+    public UserService(IUserRepository users)
+    {
+        _users = users;
+    }
+
+    public Task<User> RegisterAsync(string email, string password, string displayName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User?> GetUserByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Profile updates
+    public Task UpdateDisplayNameAsync(Guid userId, string newDisplayName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateEmailAsync(Guid userId, string newEmail)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdatePasswordAsync(Guid userId, string currentPassword, string newPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Account management
+    public Task DeleteAccountAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- src/SplitCents.Infrastructure/Repositories/UserRepository.cs
+++ src/SplitCents.Infrastructure/Repositories/UserRepository.cs
@@ -1,5 +1,31 @@
+namespace SplitCents.Infrastructure.Repositories;
 using SplitCents.Core.Interfaces.Repositories;
+using SplitCents.Core.Models;
 
-public class UserRepository : IUserRepositroy {
-    
+public class UserRepository : IUserRepository 
+{
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
 }
